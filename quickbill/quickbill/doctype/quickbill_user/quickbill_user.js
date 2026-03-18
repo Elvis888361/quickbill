a1ea0a0f8b76317d18588ce21b2f4ef3eb434bb9
Elvis888361 <elvisndegwa90@gmail.com>
2026-03-18 21:18:21 +0300

    quickbill user updates

---
+++ quickbill/quickbill/doctype/quickbill_user/quickbill_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Quickbill and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Quickbill User", {
+// 	refresh(frm) {
+
+// 	},
+// });
